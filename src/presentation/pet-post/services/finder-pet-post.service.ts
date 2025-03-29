b8863ea1391d0b381326fa0f10fb4c65adee2699
@JhonnasDEV <97009831+JhonnasMr@users.noreply.github.com>
2025-03-29 11:22:34 -0500

    changes in pet-post and pet-posts services

--- src/presentation/pet-post/services/finder-pet-post.service.ts
+++ src/presentation/pet-post/services/finder-pet-post.service.ts
@@ -1,24 +1,44 @@
-import { PetPostModel } from "../../../data";
+import { PetPostModel, UserModel } from "../../../data";
 import { CustomError } from "../../../domain";
 
 export class FinderPetPostService {
 
     async execute(id: string) {
 
-        const post = await PetPostModel.find({
-            select: ['created_at', 'description', 'hasFounded', 'id', 'user_id', 'img_url', 'pet_name'],
-            where: {
-                id: id,
-                hasFounded: false
+        try {
+
+            const post = await PetPostModel.findOne({
+                select: ['created_at', 'description', 'hasFounded', 'id', 'user_id', 'img_url', 'pet_name'],
+                where: {
+                    id: id,
+                    hasFounded: false
+                }
+            });
+
+            if (!post) {
+                throw CustomError.notFound('post dont exist');
             }
-        });
 
-        if (!post.length) {
-            throw CustomError.notFound('post dont exist');
-        }
-        else {
+            const user = await UserModel
+                .findOne({
+                    select: ['email', 'id', 'name'],
+                    where: {
+                        id: post.user_id,
+                        status: true,
+                    }
+                });
 
-            return post;
+            if (!user) {
+                throw CustomError
+                    .internalServer('the post does not have an editor');
+            }
+
+            return { user, post };
+
+        } catch (error) {
+
+            throw CustomError
+                .internalServer('something went very wrong!');
 
         }
 
--- src/presentation/pet-post/services/finder-pet-posts.service.ts
+++ src/presentation/pet-post/services/finder-pet-posts.service.ts
@@ -1,25 +1,47 @@
-import { PetPostModel, PetPostStatus } from "../../../data";
+import { PetPostModel, PetPostStatus, UserModel } from "../../../data";
 import { CustomError } from "../../../domain";
 
 export class FinderPetPostsService {
 
     async execute() {
 
-        const post = await PetPostModel.find({
-            select: ['created_at', 'description', 'hasFounded', 'id', 'user_id', 'img_url', 'pet_name'],
-            where: {
-                status: PetPostStatus.pending
-            }
-        });
-
         try {
 
-            if (!post.length) return CustomError.notFound('posts not found');
-            return post;
+            const posts = await PetPostModel.find({
+                select: ['created_at', 'description', 'hasFounded', 'id', 'user_id', 'img_url', 'pet_name'],
+                where: {
+                    status: PetPostStatus.pending
+                }
+            });
+
+
+            if (!posts.length) return CustomError.notFound('posts not found');
+
+            return this.getOwnersPost(posts);
 
         } catch (error) {
             CustomError.internalServer('somethig went worng!');
         }
+
+    }
+
+
+    private async getOwnersPost(posts: PetPostModel[]) {
+
+        let owner = [];
+
+        for (let post in posts) {
+            const user = await UserModel.findOne({
+                select: ['email', 'id', 'name'],
+                where: {
+                    id: posts[post].user_id
+                }
+            })
+            if (!user) throw CustomError.internalServer('the post does not have an editor');
+            owner.push({ user, post: posts[post] });
+        }
+
+        return owner;
 
     }
 
